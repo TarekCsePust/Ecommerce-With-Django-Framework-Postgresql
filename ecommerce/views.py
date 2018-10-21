0f45a9385c4907fe56a7a4aa82c6959efc226d84
MD. Hasan Tarek <hasantarek12cse@gmail.com>
2018-10-21 22:02:22 +0600

    first commit

---
+++ ecommerce/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import HttpResponse,render,redirect
+from django.http import JsonResponse
+from .forms import ContactForm,LoginForm,RegisterForm
+from django.contrib.auth import authenticate, login, get_user_model
+
+def home_page(request):
+    context = {
+        "title":"home page",
+        "content":"welcome to home page"
+    }
+    if request.user.is_authenticated:
+        context["premium_content"] = "YEAHHHHHH"
+
+    return render(request,'home_page.html',context)
+
+def about_page(request):
+    context = {
+        "title": "about page",
+        "content":"welcome to about page"
+    }
+    return render(request,'home_page.html',context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title":"Contact",
+        "content":" Welcome to the contact page.",
+        "form": contact_form,
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+        if request.is_ajax():
+            return JsonResponse({"message": "Thank you for your submission"})
+
+    if contact_form.errors:
+        errors = contact_form.errors.as_json()
+        if request.is_ajax():
+            return HttpResponse(errors, status=400, content_type='application/json')
+
+    # if request.method == "POST":
+    #     #print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
+
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    print(request.user.is_authenticated)
+    if form.is_valid():
+        print(form.cleaned_data)
+        username  = form.cleaned_data.get("username")
+        password  = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        #print(user)
+        #print(request.user.is_authenticated)
+        if user is not None:
+           # print(request.user.is_authenticated)
+            login(request, user)
+            # Redirect to a success page.
+            #context['form'] = LoginForm()
+            return redirect("/")
+        else:
+            # Return an 'invalid login' error message.
+            context['form'] = LoginForm()
+            #print("Error")
+
+    return render(request, "auth/login.html", context)
+
+
+
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username  = form.cleaned_data.get("username")
+        email  = form.cleaned_data.get("email")
+        password  = form.cleaned_data.get("password")
+        new_user  = User.objects.create_user(username, email, password)
+        context['form'] = RegisterForm()
+        print(new_user)
+
+    return render(request, "auth/register.html", context)
+
+
+
+
+def home_page_old(request):
+    html_ = """
+    <!doctype html>
+        <html lang="en">
+          <head>
+            <!-- Required meta tags -->
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+            <!-- Bootstrap CSS -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
+            <title>Hello, world!</title>
+          </head>
+          <body>
+            <h1>Hello, world!</h1>
+        
+            <!-- Optional JavaScript -->
+            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+          </body>
+        </html>
+    """
+    return HttpResponse(html_)
---
+++ products/views.py
@@ -0,0 +1,143 @@
+from django.shortcuts import render,get_object_or_404,Http404
+from django.views.generic import ListView,DetailView
+from .models import Product
+from carts.models import Cart
+from django.contrib.auth.mixins import LoginRequiredMixin
+from analytics.mixins import ObjectViewedMixin
+
+# Create your views here.
+
+
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all().featured()
+
+
+
+
+class UserProductHistoryView(LoginRequiredMixin, ListView):
+    template_name = "products/user-history.html"
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserProductHistoryView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        views = request.user.objectviewed_set.by_model(Product, model_queryset=False)
+        return views
+
+class ProductFeaturedDetailView(ObjectViewedMixin,DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     return Product.objects.featured()
+
+
+
+
+
+class ProductListView(ListView):
+    #queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/list.html", context)
+
+
+class ProductDetailView(ObjectViewedMixin,DetailView):
+    #queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     return Product.objects.filter(pk=pk)
+
+
+
+
+class ProductDetailSlugView(ObjectViewedMixin,DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
+    #optional for learning if we use this don't requrd first line queryset
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+        #instance = get_object_or_404(Product, slug=slug, active=True)
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist:
+            raise Http404("Not found..")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404("Uhhmmm ")
+        return instance
+
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    # instance = Product.objects.get(pk=pk) #id
+    # instance = get_object_or_404(Product, pk=pk)
+    # try:
+    #     instance = Product.objects.get(id=pk)
+    # except Product.DoesNotExist:
+    #     print('no product here')
+    #     raise Http404("Product doesn't exist")
+    # except:
+    #     print("huh?")
+
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404("Product doesn't exist")
+    # print(instance)
+    # qs  = Product.objects.filter(id=pk)
+
+    # #print(qs)
+    # if qs.exists() and qs.count() == 1: # len(qs)
+    #     instance = qs.first()
+    # else:
+    #     raise Http404("Product doesn't exist")
+
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
